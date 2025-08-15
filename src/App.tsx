429b927826b769d53ea96c5da98bbfc45abd019c
Jaysinh Chauhan <workmailofjay@gmail.com>
2025-08-15 20:44:45 +0530

    fixing bug of delete account page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
-import Contact from "./pages/Contact.tsx";
+import Contact from "./pages/Contact";
 import Privacy from "./pages/Privacy";
 import DeleteAccount from "./pages/DeleteAccount";
 import NotFound from "./pages/NotFound";
